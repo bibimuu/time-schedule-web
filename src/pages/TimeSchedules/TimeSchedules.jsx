f738e882ff5ebbe0d2188a45f020bb48bc853636
平松大悟朗 <h.dai56@gmail.com>
2020-07-03 23:42:59 +0900

    implement schedulesCard

--- src/pages/TimeSchedules/TimeSchedules.jsx
+++ src/pages/TimeSchedules/TimeSchedules.jsx
@@ -1,48 +1,47 @@
 import React from "react";
-import { Data } from '../data.js';
+import { Data } from "../data.js";
+import { ScheduleCard } from "../../components/SchedulesCard";
 
-import './style.css'
+import "./style.css";
 
 export const TimeSchedules = () => {
   console.log("time schedule page render!");
   // const weekContainer = {
-    
+
   // }
 
   const days = {
-    mon: '月',
-    tue: '火',
-    wed: '水',
-    thu: '木',
-    fri: '金',
-  }
+    mon: "月",
+    tue: "火",
+    wed: "水",
+    thu: "木",
+    fri: "金",
+  };
 
   return (
     <div>
       <h1>時間割りページ</h1>
       <div className="schedulesContainer">
-        {
-          Object.keys(days).map(day => {
-            return (
+        {Object.keys(days).map((day) => {
+          return (
+            <div>
+              <div>{days[day]}</div>
               <div>
-                <div>{days[day]}</div>
-                <div>
-                  {
-                    [...Array(5)].map((_, i) => {
-                      return <div>
-                        { 
-                          Data[day] ? 
-                            Data[day].find(d => d.time === i + 1) ? 
-                            Data[day].find(d => d.time === i + 1).title
-                            : 'none'
-                          : 'nothing' }
-                      </div>
-                    })
-                  }
-                </div>
-              </div>)
-          })
-        }
+                {[...Array(5)].map((_, i) => {
+                  return (
+                    <ScheduleCard>
+                      {Data[day]
+                        ? Data[day].find((d) => d.time === i + 1)
+                          ? Data[day].find((d) => d.time === i + 1).title
+                          : "none"
+                        : "nothing"}
+                    </ScheduleCard>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );

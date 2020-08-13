d76a8f48fded20651245493707931c01166640ef
平松大悟朗 <h.dai56@gmail.com>
2020-08-13 20:09:14 +0900

    fix: firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+import 'firebase/firebase-firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API,
+  authDomain: process.env.REACT_APP_DOMAIN,
+  databaseURL: process.env.REACT_APP_URL,
+  projectId: process.env.REACT_APP_ID,
+  storageBucket: process.env.REACT_APP_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+console.log(firebaseConfig);
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
